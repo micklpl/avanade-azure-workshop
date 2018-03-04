b6d68bfe2d35fec3dcd62b3958a51d66289f54ef
mickl <michal.stefan.klusak@gmail.com>
2018-03-04 20:22:51 +0100

    Static content (video) added for alerts tab

--- src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Controllers/HomeController.cs
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Controllers/HomeController.cs
@@ -13,10 +13,8 @@ namespace Avanade.AzureWorkshop.WebApp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Music()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
