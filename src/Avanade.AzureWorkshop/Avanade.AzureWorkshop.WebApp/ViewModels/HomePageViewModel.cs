4fbd5acaa842330a8392b747e1f08d37b8b31e72
mickl <michal.stefan.klusak@gmail.com>
2018-03-10 20:08:40 +0100

    Displaying groups on home page

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avanade.AzureWorkshop.WebApp.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public List<GroupViewModel> Groups { get; set; }
+    }
+}
