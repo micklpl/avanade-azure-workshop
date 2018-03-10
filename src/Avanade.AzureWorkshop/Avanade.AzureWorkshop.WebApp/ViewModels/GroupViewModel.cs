5fd57bd985a925117d2b829b280d3400965575c2
mickl <michal.stefan.klusak@gmail.com>
2018-03-10 16:17:41 +0100

    Groups csv generator added

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/ViewModels/GroupViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avanade.AzureWorkshop.WebApp.ViewModels
+{
+    public class GroupViewModel
+    {
+        public char GroupLetter { get; set; }
+        public List<GroupTeamViewModel> Teams { get; set; }
+    }
+
+    public class GroupTeamViewModel
+    {
+        public string Name { get; set; }
+        public string Flag { get; set; }
+    }
+}
--- src/Avanade.AzureWorkshop/Avanade.AzureWorkshop/Program.cs
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Avanade.AzureWorkshop.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Avanade.AzureWorkshop
+namespace Avanade.AzureWorkshop.Utils
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GroupsParser.Run();
         }
     }
 }
