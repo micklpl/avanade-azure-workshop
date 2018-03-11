c8b541eed8924e86041d0db07975bd137e0f151f
mickl <michal.stefan.klusak@gmail.com>
2018-03-11 12:13:11 +0100

    Team details view added

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/BusinessLogic/PlayersService.cs
@@ -0,0 +1,36 @@
+﻿using Avanade.AzureWorkshop.WebApp.Services;
+using Avanade.AzureWorkshop.WebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avanade.AzureWorkshop.WebApp.BusinessLogic
+{
+    public class PlayersService
+    {
+        private readonly CsvReader _csvReader;
+        private readonly string playersData = "Avanade.AzureWorkshop.WebApp.Resources.players.csv";
+
+        public PlayersService(CsvReader csvReader)
+        {
+            _csvReader = csvReader;
+        }
+
+        public TeamViewModel GetTeamDetails(string teamId)
+        {
+            var players = from player in _csvReader.ReadPlayers(playersData)
+                          where player.TeamId == teamId
+                          select new PlayerDetails()
+                          {
+                              Club = player.Club,
+                              DateOfBirth = player.DateOfBirth,
+                              FullName = player.FullName,
+                              Number = player.Number,
+                              Position = player.Position
+                          };
+
+            return new TeamViewModel() { Players = players.ToList() };
+        }
+    }
+}
---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/ViewModels/TeamViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avanade.AzureWorkshop.WebApp.ViewModels
+{
+    public class TeamViewModel
+    {
+        public List<PlayerDetails> Players { get; set; }
+    }
+
+    public class PlayerDetails
+    {
+        public int? Number { get; set; }
+        public string Position { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { private get; set; }
+        public int Age { get { return DateTime.Now.Year - DateOfBirth.Year; } }
+        public string Club { get; set; }
+    }
+}
