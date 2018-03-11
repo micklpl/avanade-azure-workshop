ebea2797eede1b4a58bbb8a57eeea8ceea0b0283
mickl <michal.stefan.klusak@gmail.com>
2018-03-11 12:13:11 +0100

    Team details view added

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Controllers/TeamsController.cs
@@ -0,0 +1,25 @@
+﻿using Avanade.AzureWorkshop.WebApp.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Avanade.AzureWorkshop.WebApp.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly PlayersService _playersService;
+
+        public TeamsController(PlayersService playersService)
+        {
+            _playersService = playersService;
+        }
+
+        // GET: Teams
+        public ActionResult Index(string id)
+        {
+            return View(_playersService.GetTeamDetails(id));
+        }
+    }
+}
