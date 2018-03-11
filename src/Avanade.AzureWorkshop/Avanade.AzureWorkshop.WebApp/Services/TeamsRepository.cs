e8095134001d8078aceb51c94f075639318927e8
mickl <michal.stefan.klusak@gmail.com>
2018-03-11 13:45:55 +0100

    Placeholders for table storage

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Services/TeamsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avanade.AzureWorkshop.WebApp.Services
+{
+    public class TeamsRepository
+    {
+    }
+}
