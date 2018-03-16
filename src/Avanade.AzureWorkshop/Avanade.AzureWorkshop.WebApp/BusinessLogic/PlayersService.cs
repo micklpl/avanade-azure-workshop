4d2f67228fa737e083e5cea6df77b53e8b781397
mickl <michal.stefan.klusak@gmail.com>
2018-03-16 20:34:03 +0100

    Blobs logic template added

--- src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/BusinessLogic/PlayersService.cs
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/BusinessLogic/PlayersService.cs
@@ -3,6 +3,7 @@ using Avanade.AzureWorkshop.WebApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Avanade.AzureWorkshop.WebApp.BusinessLogic
@@ -11,11 +12,14 @@ namespace Avanade.AzureWorkshop.WebApp.BusinessLogic
     {
         private readonly TeamsRepository _teamsRepository;
         private readonly ImagesService _imagesService;
+        private readonly BinaryFilesRepository _binaryFilesRepository;
 
-        public PlayersService(TeamsRepository teamsRepository, ImagesService imagesService)
+        public PlayersService(TeamsRepository teamsRepository, 
+            ImagesService imagesService, BinaryFilesRepository binaryFilesRepository)
         {
             _teamsRepository = teamsRepository;
             _imagesService = imagesService;
+            _binaryFilesRepository = binaryFilesRepository;
         }
 
         public TeamViewModel GetTeamDetails(string teamId)
@@ -38,7 +42,7 @@ namespace Avanade.AzureWorkshop.WebApp.BusinessLogic
         public PlayerDetails GetPlayerDetails(string teamId, string playerId)
         {
             var player = _teamsRepository.FetchSinglePlayer(teamId, playerId);
-            var images = _imagesService.SearchForImages(player.FullName);
+
             return new PlayerDetails()
             {
                 Club = player.Club,
@@ -47,8 +51,26 @@ namespace Avanade.AzureWorkshop.WebApp.BusinessLogic
                 Number = player.Number,
                 Position = player.Position,
                 PlayerId = player.RowKey,
-                Images = images.ToList()
+                Images = GetPlayerImages(player.FullName, player.RowKey, player.PartitionKey).Result
             };
+        }
+
+        private async Task<List<string>> GetPlayerImages(string fullName, string playerId, string teamId)
+        {
+            if (await _binaryFilesRepository.FileExists(teamId, playerId))
+            {
+                return await _binaryFilesRepository.GetBlobUrls(teamId, playerId);
+            }
+
+            var images = _imagesService.SearchForImages(fullName).ToList();
+
+            foreach(var image in images)
+            {
+                byte[] data = _imagesService.DownloadImage(image);
+                await _binaryFilesRepository.SaveBlob(teamId, playerId, data);
+            }
+
+            return images;
         }
     }
 }
