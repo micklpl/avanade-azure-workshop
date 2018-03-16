5e4f1e50828f59d732fc87dfd267817d7c89521c
mickl <michal.stefan.klusak@gmail.com>
2018-03-16 20:34:03 +0100

    Blobs logic template added

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Services/BinaryFilesRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Avanade.AzureWorkshop.WebApp.Services
+{
+    public class BinaryFilesRepository
+    {
+        public async Task<bool> FileExists(string containerName, string fileName)
+        {
+            await Task.Delay(0);
+            return false;
+        }
+
+        public async Task SaveBlob(string containerName, string fileName, byte[] bytes)
+        {
+            await Task.Delay(0);
+        }
+
+        public async Task<List<string>> GetBlobUrls(string containerName, string fileName)
+        {
+            await Task.Delay(0);
+            return Enumerable.Empty<string>().ToList();
+        }
+    }
+}
