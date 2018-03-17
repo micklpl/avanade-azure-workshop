43181d465c86aefd42785e0e45aeb6cd55204bfa
AVANADE-CORP\krzysztof.lelito <krzysztof.lelito@avanade.com>
2018-03-17 17:20:45 +0100

    Add topic service and send message to service bus.

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Models/ServiceBusModels/BaseMessageModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.Serialization;
+
+namespace Avanade.AzureWorkshop.WebApp.Models.ServiceBusModels
+{
+    [DataContract]
+    public class BaseMessageModel
+    {
+    }
+}
---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Models/ServiceBusModels/GameMessageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Avanade.AzureWorkshop.WebApp.Models.ServiceBusModels
+{
+    public class GameMessageModel : BaseMessageModel
+    {
+        [DataMember]
+        public string Group { get; set; }
+        public string Team1Name { get; set; }
+        public string Team2Name { get; set; }
+        public int Team1Goals { get; set; }
+        public int Team2Goals { get; set; }
+        public IEnumerable<string> Team1Scorers { get; set; }
+        public IEnumerable<string> Team2Scorers { get; set; }
+        public DateTime DateOfGame { get; set; }
+    }
+}
