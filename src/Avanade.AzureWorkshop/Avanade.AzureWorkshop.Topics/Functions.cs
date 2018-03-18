93f5dcdc933f39f2e92a00852aac75d33cd21054
AVANADE-CORP\krzysztof.lelito <krzysztof.lelito@avanade.com>
2018-03-18 12:38:36 +0100

    Add web job to web app. Consume GameMessageModel

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.Topics/Functions.cs
@@ -0,0 +1,28 @@
+﻿using Avanade.AzureWorkshop.WebApp.Models.ServiceBusModels;
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Avanade.AzureWorkshop.Topics
+{
+    public class Functions
+    {
+        private const string SubscriptionName = "webjobssubscription";
+
+        public async Task ProcessGameMessage([ServiceBusTrigger(nameof(GameMessageModel), SubscriptionName)] GameMessageModel message, TextWriter textWriter)
+        {
+            await ProcessTopic(message, textWriter);
+        }
+
+        private async Task ProcessTopic<TTopic>(TTopic message, TextWriter textWriter) where TTopic : BaseMessageModel
+        {
+            await WriteMessage($"Processing topic message {typeof(TTopic).Name}. Body: {JsonConvert.SerializeObject(message)}", textWriter);
+        }
+
+        private static async Task WriteMessage(string message, TextWriter writer)
+        {
+            await writer.WriteLineAsync(message);
+        }
+    }
+}
