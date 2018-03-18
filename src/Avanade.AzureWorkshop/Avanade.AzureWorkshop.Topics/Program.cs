ab501ebac948fc3db2df854abb72a0822512403d
AVANADE-CORP\krzysztof.lelito <krzysztof.lelito@avanade.com>
2018-03-18 12:38:36 +0100

    Add web job to web app. Consume GameMessageModel

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.Topics/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace Avanade.AzureWorkshop.Topics
+{
+    internal class Program
+    {
+        private static void Main()
+        { 
+            var config = new JobHostConfiguration();
+            config.UseServiceBus();
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
