b3a28ab1e84fa0c1466cacb523bb7063984993af
AVANADE-CORP\krzysztof.lelito <krzysztof.lelito@avanade.com>
2018-03-24 15:45:59 +0100

    Send email using Mailgun

---
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/BusinessLogic/NewsletterService.cs
@@ -0,0 +1,20 @@
+﻿using Avanade.AzureWorkshop.WebApp.Services;
+using System.Threading.Tasks;
+
+namespace Avanade.AzureWorkshop.WebApp.BusinessLogic
+{
+    public class NewsletterService
+    {
+        private readonly MailgunService _mailgunService;
+
+        public NewsletterService(MailgunService mailgunService)
+        {
+            _mailgunService = mailgunService;
+        }
+
+        public async Task SendNewsletter()
+        {
+            await Task.FromResult<object>(_mailgunService.SendEmail());
+        }
+    }
+}
