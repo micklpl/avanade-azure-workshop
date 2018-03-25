de71ca9d9ca659f193a6f8de3613433e110e379f
mickl <michal.stefan.klusak@gmail.com>
2018-03-25 15:59:11 +0200

    Telemetry Service placeholder

--- src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Services/TelemetryService.cs
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Services/TelemetryService.cs
@@ -9,16 +9,8 @@ namespace Avanade.AzureWorkshop.WebApp.Services
 {
     public class TelemetryService
     {
-        private readonly TelemetryClient _telemetryClient;
-
-        public TelemetryService()
-        {
-            _telemetryClient = new TelemetryClient();
-        }
-
         public void Log(string message, string correlationId)
         {
-            _telemetryClient.TrackTrace($"{message}, {nameof(correlationId)}: {correlationId}");
         }
     }
 }
