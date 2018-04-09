88314e7b2f4509f5085c2dd87541c66af643f0c4
michal.klusak <michal.klusak@WIN-1VBCUJPE4Y>
2018-04-09 18:32:00 +0200

    Application Insights migrated to 2.4.1

--- src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Models/ServiceBusModels/BaseMessageModel.cs
+++ src/Avanade.AzureWorkshop/Avanade.AzureWorkshop.WebApp/Models/ServiceBusModels/BaseMessageModel.cs
@@ -2,10 +2,8 @@
 
 namespace Avanade.AzureWorkshop.WebApp.Models.ServiceBusModels
 {
-    [DataContract]
     public class BaseMessageModel
     {
-        [DataMember]
         public string CorrelationId { get; set; }
     }
 }
